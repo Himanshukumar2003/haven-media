f5f1459f716a9a948c93869a45474c480ba984a9
himanshu <devliyalhimanshu@gmail.com>
2024-11-24 16:26:13 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -1,17 +1,5 @@
 
 
-window.onscroll = function () {
-  const navbar = document.getElementById("navbar");
-
-  if (window.scrollY > 100) {
-    navbar.classList.remove("transparent");
-  } else {
-    navbar.classList.add("transparent");
-  }
-};
-
-
-
 // Handle navbar transparency on scroll
 window.onscroll = function () {
   const navbar = document.getElementById("navbar");
@@ -20,11 +8,6 @@ window.onscroll = function () {
   // Check if menu is open
   const isMenuOpen = navbarToggler.getAttribute("aria-expanded") === "true";
 
-  if (window.scrollY > 100 || isMenuOpen) {
-    navbar.classList.remove("transparent");
-  } else {
-    navbar.classList.add("transparent");
-  }
 };
 
 
@@ -32,17 +15,7 @@ $(function () {
   $(".navbar-toggler").on("click", function () {
     const navbar = document.getElementById("navbar");
 
-    // Toggle the 'open' class for the button (optional visual effect)
     $(this).toggleClass("open");
-
-    // Change navbar background color based on menu state
-    if ($(this).attr("aria-expanded") === "true") {
-      navbar.classList.add("bg-white"); // Add white background
-      navbar.classList.remove("bg-red-color"); // Remove red background
-    } else {
-      navbar.classList.remove("bg-white"); // Remove white background
-      navbar.classList.add("bg-red-color"); // Add red background
-    }
   });
 });
 

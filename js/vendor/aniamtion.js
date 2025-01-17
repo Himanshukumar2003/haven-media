d9382512ec1546a32caada37cef76941fa5ae508
himanshu <devliyalhimanshu@gmail.com>
2025-01-17 18:44:38 +0530

    first commit

---
+++ js/vendor/aniamtion.js
@@ -0,0 +1,29 @@
+
+// Wrap every letter in a span
+document.querySelectorAll('.ml9 .letters').forEach((element) => {
+    element.innerHTML = element.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+});
+
+// Animate text
+anime.timeline({ loop: false })
+    .add({
+        targets: '.ml9 .letter:not(.highlighter)', // Exclude highlighter letters
+        scale: [0, 1],
+        duration: 1500,
+        elasticity: 600,
+        delay: (el, i) => 45 * (i + 1),
+    })
+    .add({
+        targets: '.ml9',
+        opacity: 1,
+        duration: 1000,
+        easing: 'easeOutExpo',
+    });
+
+
+
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml11 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+

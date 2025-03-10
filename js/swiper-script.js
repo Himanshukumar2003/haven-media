264269120eb09c8586e7e63e23bae12550515b7a
Himanshu Kumar <devliyalhimanshu@gmail.com>
2025-03-10 13:00:00 +0530

    first commit

--- js/swiper-script.js
+++ js/swiper-script.js
@@ -1,59 +1,19 @@
-$(function () {
-    var swiperTestimonials = new Swiper('#clients-swiper', {
-        autoplay: {
-            delay: 3000,
-        },
-        speed: 1000,
-        slidesPerView: 1,
-        slidesPerGroup: 1,
-        spaceBetween: 10,
-        loop: true,
-        dots: true,
-        grabCursor: true,
-        breakpoints: {
-            360: {
-                slidesPerView: 1,
-            },
-            768: {
-                slidesPerView: 2,
-            },
-            1024: {
-                slidesPerView: 4,
-            },
-        },
-        navigation: {
-            nextEl: '.swiper-next-btn',
-            prevEl: '.swiper-prev-btn',
-        },
-        pagination: {
-            el: '.swiperPagination',
-            clickable: true,
-        },
-        on: {
-            slideChange: function () {
-                updateNavigationButtons(this);
-            },
-            init: function () {
-                updateNavigationButtons(this); // Ensure buttons are updated on initialization
-            },
-        },
+  document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper("#clients-swiper", {
+      loop: true, 
+      spaceBetween: 20, 
+      slidesPerView: 4, 
+      navigation: {
+        nextEl: ".swiper-next-btn",
+        prevEl: ".swiper-prev-btn",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      autoplay: {
+        delay:800, 
+        disableOnInteraction: false,
+      },
     });
-
-    function updateNavigationButtons(swiper) {
-        const prevButton = $(swiper.navigation.prevEl);
-        const nextButton = $(swiper.navigation.nextEl);
-
-        // Enable or disable the "Previous" button
-        if (swiper.isBeginning) {
-            prevButton.addClass('disabled').prop('disabled', true);
-        } else {
-            prevButton.removeClass('disabled').prop('disabled', false);
-        }
-
-        if (swiper.isEnd) {
-            nextButton.addClass('disabled').prop('disabled', true);
-        } else {
-            nextButton.removeClass('disabled').prop('disabled', false);
-        }
-    }
-});
+  });

e20fb576050f7211019db4a7c9e07c7e5d8147f8
Himanshu Kumar <devliyalhimanshu@gmail.com>
2025-03-15 17:42:00 +0530

    first commit

--- js/swiper-script.js
+++ js/swiper-script.js
@@ -1,19 +1,27 @@
-  document.addEventListener("DOMContentLoaded", function () {
-    var swiper = new Swiper("#clients-swiper", {
-      loop: true, 
-      spaceBetween: 20, 
-      slidesPerView: 4, 
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper("#clients-swiper", {
+      loop: false,
+      spaceBetween: 20,
+      slidesPerView: 4,
       navigation: {
-        nextEl: ".swiper-next-btn",
-        prevEl: ".swiper-prev-btn",
+          nextEl: ".swiper-next-btn",
+          prevEl: ".swiper-prev-btn",
       },
       pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+          el: ".swiper-pagination",
+          clickable: true,
       },
-      autoplay: {
-        delay:800, 
-        disableOnInteraction: false,
-      },
-    });
+      autoplay: false, 
+      breakpoints: {
+        
+          0: {   // Medium screens
+              slidesPerView: 2,
+              spaceBetween: 20
+          },
+          1024: {  // Large screens
+              slidesPerView: 4,
+              spaceBetween: 20
+          }
+      }
   });
+});

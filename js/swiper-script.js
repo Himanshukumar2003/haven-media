b5cb10d99d67eebb77b851907e14042f327d5ff1
Himanshu Kumar <devliyalhimanshu@gmail.com>
2025-03-20 18:33:38 +0530

    first commit

--- js/swiper-script.js
+++ js/swiper-script.js
@@ -1,28 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var swiper = new Swiper("#clients-swiper", {
-      loop: false,
-      spaceBetween: 20,
-      slidesPerView: 4,
-      navigation: {
-          nextEl: ".swiper-next-btn",
-          prevEl: ".swiper-prev-btn",
-      },
-      pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-      },
-      autoplay: {
-          delay: 900, 
-      },
-      breakpoints: {
-          0: {   
-              slidesPerView: 2,
-              spaceBetween: 20
-          },
-          1024: {  
-              slidesPerView: 4,
-              spaceBetween: 20
-          }
-      }
-  });
-});
+    document.addEventListener("DOMContentLoaded", function () {
+        var swiper = new Swiper("#clients-swiper", {
+            loop: true,
+            slidesPerView: 3,
+            spaceBetween: 20,
+            autoplay: {
+                delay: 3000,
+                disableOnInteraction: false
+            },
+            breakpoints: {
+                768: {
+                    slidesPerView: 4
+                },
+                1024: {
+                    slidesPerView: 5
+                }
+            },
+            on: {
+                init: function () {
+                    AOS.refresh(); 
+                },
+                slideChangeTransitionEnd: function () {
+                    AOS.refresh(); 
+                }
+            }
+        });
+
+        AOS.init(); 
+    });
